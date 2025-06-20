cde4d5e295311ae659608f23d652a3b961e65b9b
MarleneAmi-612 <iamfox.28@gmail.com>
2025-06-20 10:05:36 -0600

    Componentes usando router dom

--- frontend/frontprac/src/modules/user/UserForm.tsx
+++ frontend/frontprac/src/modules/user/UserForm.tsx
@@ -1,48 +1,60 @@
-import React from 'react';
-import { Form, Input } from 'antd';
+import React from 'react'
 
-const App: React.FC = () => (
-  <>
+import { Form, Input, Button } from 'antd';
+export default function UserForm() {
+
+
+  const [formUser] = Form.useForm(); //Controlar el fromulario
+
+  const handleSubmit = () => {
+    const values = formUser.getFieldsValue(); //obtener todos los datos en JSON
+
+    console.log('Todos los datos del formulario: ', values)
+
+  
+  };
+
+  
+  return (
+    <div>
     <Form
       name="layout-multiple-horizontal"
       layout="horizontal"
+      form={formUser}
       labelCol={{ span: 4 }}
       wrapperCol={{ span: 20 }}
     >
-      <Form.Item label="horizontal" name="horizontal" rules={[{ required: true }]}>
+      <Form.Item label="Username" name="Username" rules={[{ required: true }]}>
         <Input />
       </Form.Item>
-      <Form.Item
-        layout="vertical"
-        label="vertical"
-        name="vertical"
-        rules={[{ required: true }]}
-        labelCol={{ span: 24 }}
-        wrapperCol={{ span: 24 }}
-      >
-        <Input />
-      </Form.Item>
-    </Form>
-    <br />
-    <Form
-      name="layout-multiple-vertical"
-      layout="vertical"
-      labelCol={{ span: 4 }}
-      wrapperCol={{ span: 20 }}
-    >
-      <Form.Item label="vertical" name="vertical" rules={[{ required: true }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item
-        layout="horizontal"
-        label="horizontal"
-        name="horizontal"
-        rules={[{ required: true }]}
-      >
-        <Input />
-      </Form.Item>
-    </Form>
-  </>
-);
 
-export default App;
+      <Form.Item label="Password" name="Password" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Email" name="E-mail" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
+
+      
+
+      <br></br>
+
+
+      <Form.Item>
+        <Button onClick={handleSubmit}>
+
+          Enviar
+
+        </Button>
+
+      </Form.Item>
+
+
+
+    </Form>
+
+      
+    </div>
+  )
+}
